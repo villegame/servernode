2c5cb661657468de62da2407a37116d4d5bafc81
villegame <ville.niskavaara@gmail.com>
2015-10-27 09:48:55 +0200

    Tidied up code.

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-//var playerIds = [];
-//var playerSockets = [];
+// Player data
+// contains: var player = {playerId: id, playerSocket: socket, playerName: name, playerKills: kills, playerDeaths: deaths};
+var playerData = []; 
 
-var playerData = []; // contains: var player = {playerId: id, playerSocket: socket, playerName: name, playerKills: kills, playerDeaths: deaths};
+// Game statistics data
+// contains: var data = {playerName: name, playerKills: kills, playerDeaths: deaths};
+var statsData = []; 
 
-var statsData = []; // contains: var data = {playerName: name, playerKills: kills, playerDeaths: deaths};
-
-var presentWreckages = [];
+// Present wreckage positions on game area
 // contains: var wreckage = {wreckId:, playerName:, killerName:, wreckX:, wreckY:, wreckZ:};
+var presentWreckages = [];
 
-//io.sockets.on('connection', function(socket) {
+//////////////////////////////////////////
+//
+// MESSAGE HANDLING:
+//
+//////////////////////////////////////////
+
+// New connection
 io.on('connection', function(socket) {
     console.log('client connected, sending message...');
     
@@ -22,109 +30,135 @@ io.on('connection', function(socket) {
     
     console.log('generated id ' + id + ' for player');
     
-    // Create player in player list
+    // Create player and put it in player list
     var player = {playerId: id, playerSocket: socket, playerName: "-", playerKills: 0, playerDeaths: 0};
     playerData.push(player);
     
+    // Emit generated player id for player
     socket.emit('connected', {playerId: id});
     
-    //Send present wreckages
+    //Send present wreckages to new player
     for(var i = 0; i < presentWreckages.length; i++) {
-        // send these as wreckageinputs or something else
+        // Send wreckage as death (death message enters wreckage on client)
         socket.emit('Death', presentWreckages[i]);
     }
     
-    //Send current players and statistics
-    // ToBeDone
-    
-    socket.on('disconnect', function() {
-        console.log('player ' + getIdBySocket(socket) + " disconnected");
-        io.emit('DeletePlayer', {playerId: getIdBySocket(socket)});
-        deletePlayer(socket);
-    });
-    
+    // Player sends player name
     socket.on('PlayerName', function(data) {
-        addNameToPlayer(socket, data.playerName);
-        
-        // send statistics
+        addNameToPlayer(socket, data.playerName);        
+        // As response to name send statistics
         for(var i = 0; i < playerData.length; i++) {
             io.emit('StatsData', {playerId: playerData[i].playerId, playerName: playerData[i].playerName, playerKills: playerData[i].playerKills, playerDeaths: playerData[i].playerDeaths});
         }
     });
     
-    socket.on('Hello', function(data) { // Dummy test function
-        console.log('got a hello');
-        console.log("got float: " + data.field1 + " and string: " + data.field2);
-        socket.emit("Reply");
-    });
-    
+    // Weapon launch message
     socket.on('Launch', function(data) {
         console.log('Firing... '  + getIdBySocket(socket));
         io.emit('Launch', data);
     });
     
+    // Incoming weapon explosion (shooting player sends, server broadcasts to everyone)
     socket.on('Explosion', function(data) {
-        //console.log('Explosion... ' + data.x + ' ' + data.y + ' ' + data.z);
         io.emit('Explosion', data);
     });
     
+    // Client sent scan message, broadcast to everyone
     socket.on('RadarScan', function(data) {
         console.log('RadarScan...');
         io.emit('RadarScan', data);
     });
     
+    // Client that was hit by radar scan, returns message to scanning player
     socket.on('RadarReply', function(data) {
         console.log('RadarReply...');
         var tmpSocket = getSocketByPlayerId(data.player);
         tmpSocket.emit('RadarReply', data);
     });
     
+    // Client sent death message (player died)
     socket.on('Death', function(data) {
         console.log('Death...');
         
+        // Generate wreck id
         var wreckId = "";
         wreckId += generateId();
         
         console.log('Player ' + getNameByPlayerId(data.killer) + ' destroyed player ' + getNameByPlayerId(data.player));
+        
+        // Update player statistics by adding kill and death
         addKillToPlayer(data.killer);
         addDeathToPlayer(data.player);
-        //logPlayerData();
         
-        // send statistics
+        // Send new statistics to everyone
         for(var i = 0; i < playerData.length; i++) {
             io.emit('StatsData', {playerId: playerData[i].playerId, playerName: playerData[i].playerName, playerKills: playerData[i].playerKills, playerDeaths: playerData[i].playerDeaths});
         }
         
+        // Create wreckage item and add it to wreckage list
         var wreckage = {wreckId: wreckId, playerName: getNameByPlayerId(data.player), killerName: getNameByPlayerId(data.killer), wreckX: data.x, wreckY: data.y, wreckZ: data.z};
         presentWreckages.push(wreckage);
         
+        // Emit death message for all clients
         io.emit('Death', wreckage);
     });
     
+    // Client removed wreck
     socket.on('RemoveWreck', function(data) {
         console.log('Wreck removed... ' + data.wreckId);
+        // Broadcast wreck remove
         io.emit('RemoveWreck', data);
+        // Delete wreck from wreck list
         deleteWreck(data.wreckId);
+    });
+    
+    // Player disconnected
+    socket.on('disconnect', function() {
+        console.log('player ' + getIdBySocket(socket) + " disconnected");
+        // Send delete player to other clients
+        io.emit('DeletePlayer', {playerId: getIdBySocket(socket)});
+        // Delete pleyer from player list by socket
+        deletePlayer(socket);
+    });
+    
+    // Dummy test function
+    socket.on('Hello', function(data) {
+        console.log('got a hello');
+        console.log("got float: " + data.field1 + " and string: " + data.field2);
+        socket.emit("Reply");
     });
 });
 
+//////////////////////////////////////////
+//
+// SERVER LISTENER:
+//
+//////////////////////////////////////////
+
+// Variables for openshift service
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 7777
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-// can listen just by port too..
+// Can listen just by port too as commented, ip required by openshift
 http.listen(server_port, server_ip_address,  function() {
 //http.listen(server_port, function() {
     console.log('listening to ' + server_ip_address + ":" + server_port);
 });
 
-// Generate random Id
+//////////////////////////////////////////
+//
+// FUNCTIONS:
+//
+//////////////////////////////////////////
+
+// Generate unique, random Id
 function generateId()
 {
     var id;
-    //= "";
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     do {
         id = "";
-        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        //var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
         for( var i=0; i < 10; i++ )
             id += chars.charAt(Math.floor(Math.random() * chars.length));
@@ -134,7 +168,7 @@ function generateId()
     return id;
 }
 
-// add kill to player
+// Add kill to player
 function addKillToPlayer(id) {
     for(var i = 0; i < playerData.length; i++) {
         if(id == playerData[i].playerId) {
@@ -144,7 +178,7 @@ function addKillToPlayer(id) {
     }
 }
 
-// add death to player
+// Add death to player
 function addDeathToPlayer(id) {
     for(var i = 0; i < playerData.length; i++) {
         if(id == playerData[i].playerId) {
@@ -154,7 +188,7 @@ function addDeathToPlayer(id) {
     }
 }
 
-// get socket by player id
+// Get socket by player id
 function getSocketByPlayerId(id) {
     for(var i = 0; i < playerData.length; i++) {
         if(id == playerData[i].playerId) {
@@ -164,7 +198,7 @@ function getSocketByPlayerId(id) {
     return null;
 }
 
-// get name by player id
+// Get name by player id
 function getNameByPlayerId(id) {
     for(var i = 0; i < playerData.length; i++) {
         if(id == playerData[i].playerId) {
@@ -174,18 +208,17 @@ function getNameByPlayerId(id) {
     return null;
 }
 
-// get player id by socket
+// Get player id by socket
 function getIdBySocket(socket) {
     for(var i = 0; i < playerData.length; i++) {
         if(socket == playerData[i].playerSocket) {
             return playerData[i].playerId;
         }
-    }
-    
+    }    
     return null;
 }
 
-// add player a name
+// Add player a name
 function addNameToPlayer(socket, name) {
     for(var i = 0; i < playerData.length; i++) {
         if(socket == playerData[i].playerSocket) {
@@ -195,7 +228,7 @@ function addNameToPlayer(socket, name) {
     }
 }
 
-// remove player from connected players list
+// Remove player from connected players list
 function deletePlayer(socket) {
     var indexToRemove = -1;
     for(var i = 0; i < playerData.length; i++) {
@@ -209,7 +242,7 @@ function deletePlayer(socket) {
     }    
 }
 
-// check if generated id exists
+// Check if generated id exists
 function checkIfUniqueId(id) {
     for(var i = 0; i < playerData.length; i++) {
         if(id == playerData[i].playerId) return false;
@@ -220,7 +253,7 @@ function checkIfUniqueId(id) {
     return true;
 }
 
-// remove wreck from wreck list
+// Remove wreck from wreck list
 function deleteWreck(wreckId) {
     var indexToRemove = -1;
     for(var i = 0; i < presentWreckages.length; i++) {
@@ -234,7 +267,7 @@ function deleteWreck(wreckId) {
     }    
 }
 
-// generate stats table
+// Generate stats table
 function makeStatsData() {
     statsData.splice(0,statsData.length);
     for(var i = 0; i < playerData.length; i++) {
@@ -242,7 +275,7 @@ function makeStatsData() {
     }
 }
 
-// list playerData list (all connected players)
+// List playerData list (all connected players), only server console output
 function logPlayerData() {
     console.log("players: " + playerData.length);
     for(var i = 0; i < playerData.length; i++) {
@@ -250,7 +283,7 @@ function logPlayerData() {
     }
 }
 
-// list presentWreckages list (all present wrecks)
+// List presentWreckages list (all present wrecks), only server console output
 function logWreckages() {
     console.log("wrecks: " + presentWreckages.length);
     for(var i = 0; i < presentWreckages.length; i++) {
